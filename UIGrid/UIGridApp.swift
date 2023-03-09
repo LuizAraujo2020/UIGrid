c6b8bd9667f6227a7070637591e54b437e9f7c60
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 15:51:18 -0300

    Initial Commit

---
+++ UIGrid/UIGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIGridApp.swift
+//  UIGrid
+//
+//  Created by Luiz Araujo on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UIGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
